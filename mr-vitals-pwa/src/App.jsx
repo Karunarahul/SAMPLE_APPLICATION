ee4254fdd8ecd382cf0185428ead637c5d381ba7
MKRahul <karunarahul8885@gmail.com>
2026-03-26 09:54:38 +0530

    fix: switch to HashRouter to fix blank screens on Vite base paths

--- mr-vitals-pwa/src/App.jsx
+++ mr-vitals-pwa/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Welcome from './pages/Welcome'
 import Home from './pages/Home'
 import Analysis from './pages/Analysis'
