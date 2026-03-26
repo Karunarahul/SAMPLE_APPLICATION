08dbec1f676b3a02c0e73a2bd981e39cadac20c8
MKRahul <karunarahul8885@gmail.com>
2026-03-26 09:49:44 +0530

    fix: restrict babel explicit alias to CI build only

--- mr-vitals-pwa/vite.config.js
+++ mr-vitals-pwa/vite.config.js
@@ -1,19 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
+export default defineConfig(({ command }) => ({
+    plugins: [react(), basicSsl()],
     base: '/SAMPLE_APPLICATION/',
     server: {
         host: true
     },
     resolve: {
-        alias: [
+        alias: command === 'build' ? [
             {
                 find: /^@babel\/runtime\/helpers\/esm\/(.*)$/,
                 replacement: '@babel/runtime/helpers/esm/$1.js',
             }
-        ]
+        ] : []
     }
-})
+}))
